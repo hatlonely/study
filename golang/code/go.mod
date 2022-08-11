e229f5de4b324823dddf36e9480b6c538ecc988e
hatlonely <hatlonely@gmail.com>
2022-08-12 00:22:53 +0800

    update

---
+++ golang/code/go.mod
@@ -0,0 +1,3 @@
+module github.com/hatlonely/study/golang/code
+
+go 1.16
---
+++ golang/code/text_template_test.go
@@ -0,0 +1,10 @@
+package code
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test1(t *testing.T) {
+	fmt.Println("hello world")
+}
